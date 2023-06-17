1448fdfb3878bfaad3d4316829b0ef5b64690abe
javiercastaneda303 <javiercastaneda303@gmail.com>
2023-06-17 11:03:11 +0100

    _

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import Typography from '@mui/material/Typography'
 // import './style.css'
-import { phoneWhatsApp, phoneVisible, phoneOld, email, linkedinURL, instagramURL } from "../../constant";
+import { phoneWhatsApp, phoneVisible, phoneOld, email, linkedinURL, facebookURL } from "../../constant";
 import { useNavigate } from "react-router-dom";
 require('./style.css');
 export default function Footer(){
     const [t, i18n] = useTranslation("global");
     let navigate = useNavigate();
-  const handleClickLinkedin = ()=>{
-    console.log("click")
-    navigate(linkedinURL)
-  }
+    const handleClickLinkedin = ()=>{
+      navigate(linkedinURL)
+    }
+    const handleClickFacebook = ()=>{
+      navigate(facebookURL)
+    }
 
     return (
       <div>
+        <div className="icon-footer">
+          <a target="_blank" href="https://www.facebook.com/elhierroguide"   onClick={handleClickFacebook}  rel="noreferrer">
+            <img src="/icons/facebook.png"  alt="icon-linkedin" style={{ width:'100%', cursor: 'pointer' }} />
+          </a>        
+        </div>
         <div className="icon-footer">
           <a href='mailto:jo.rudnik@gmail.com?subject=Subject&body=Body%20goes%20here'>          
             <img  src="/icons/email.png" className="_icon-language" alt="icon-email" style={{ width:'100%', cursor: 'pointer' }} />
@@ -30,11 +37,11 @@ export default function Footer(){
             <img src="/icons/whatsApp.png"  alt="icon-whatsApp" style={{ width:'100%', cursor: 'pointer' }} />
           </a>
         </div>
-        {/* <div className="icon-footer">
-          <a target="_blank" href={instagramURL}   rel="noreferrer">
+        <div className="icon-footer">
+          <a target="_blank" href={`https://www.instagram.com/el.hierro.guide/`}   rel="noreferrer">
             <img src="/icons/instagram.png"  alt="icon-instagram" style={{ width:'100%', cursor: 'pointer' }} />
           </a>
-        </div> */}
+        </div> 
         <br></br>
         
         <Typography variant="h6" color="initial" sx={{ textShadow: '2px 2px #000', color: '#fff' }}>{phoneVisible}</Typography> 

6ef52ade92a33d12cc1475ffa9aa9665fa8c4d0c
javiercastaneda303 <javiercastaneda303@gmail.com>
2024-02-17 11:44:20 +0100

    _

--- src/pages/Offer/index.tsx
+++ src/pages/Offer/index.tsx
@@ -12,8 +12,8 @@ export default function Contact(){
             {/* <img className="back-image" src="images/aboutMe.jpg"  style={{left:(width as number) <1600 ?(((16*(height as number)/12)-(width as number))/2):"50%"}}/> */}
             <img className="back-image-offer" src="images/offer.jpg" style={{left:(width as number) <70 ?(-(((12*(height as number)/16)-(width as number))/1.3)):"0"}} />
         </div>
-        <div className="background-offer" >
-          <Typography variant="h4" color="inherit" sx={{ textShadow: '1px 1px #000' }}>
+        <div className="background-offer"style={{maxWidth: '500px', textAlign: 'center'}} >
+          <Typography variant="h4" color="inherit" sx={{ textShadow: '1px 1px #000' }} >
             {t("offer.tourOperators")}
             <br />
             {t("offer.groups")}

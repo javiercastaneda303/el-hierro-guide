dee6cdbd93c356108e475be046df39a6a5707dd4
javiercastaneda303 <javiercastaneda303@gmail.com>
2024-02-17 11:45:56 +0100

    feat: los errores corregidos

--- src/pages/Offer/index.tsx
+++ src/pages/Offer/index.tsx
@@ -2,27 +2,27 @@ import { Typography } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import useWindowSize from "../../hooks/useWindowsSize";
-require('./style.css')
+require('./style.css');
+
 export default function Contact(){
+  /* eslint-disable */
     const [t, i18n] = useTranslation("global");
-    const {height,width}= useWindowSize()
+    const {height, width} = useWindowSize();
+
     return (
       <>
-        <div className="container_back-offer" >
-            {/* <img className="back-image" src="images/aboutMe.jpg"  style={{left:(width as number) <1600 ?(((16*(height as number)/12)-(width as number))/2):"50%"}}/> */}
-            <img className="back-image-offer" src="images/offer.jpg" style={{left:(width as number) <70 ?(-(((12*(height as number)/16)-(width as number))/1.3)):"0"}} />
+        <div className="container_back-offer">
+            <img className="back-image-offer" src="images/offer.jpg" style={{left: width < 700 ? (-(((12*height)/16)-width)/1.3) : "0"}} />
         </div>
-        <div className="background-offer"style={{maxWidth: '500px', textAlign: 'center'}} >
-          <Typography variant="h4" color="inherit" sx={{ textShadow: '1px 1px #000' }} >
+        <div className="background-offer">
+          <Typography variant="h4" color="inherit" sx={{ textShadow: '1px 1px #000', width: '100%', textAlign: 'center' }}>
             {t("offer.tourOperators")}
             <br />
             {t("offer.groups")}
             <br />
             {t("offer.contact")}
           </Typography>
-          <br/>          
         </div>
       </>
-    )
-    
+    );
 }
